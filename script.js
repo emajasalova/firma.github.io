07f527a13e831977425f0051da5129bc7e2c58c1
emajasalova <143315521+emajasalova@users.noreply.github.com>
2023-08-30 20:38:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const jobForm = document.getElementById("jobForm");
-    const jobList = document.querySelector(".job-list");
-  
-    jobForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const jobTitle = document.getElementById("jobTitle").value;
-      const jobDescription = document.getElementById("jobDescription").value;
-      const companyName = document.getElementById("companyName").value;
-      const phoneNumber = document.getElementById("phoneNumber").value;
-  
+  const jobForm = document.getElementById("jobForm");
+  const jobList = document.querySelector(".job-list");
+  const savedJobs = JSON.parse(localStorage.getItem("jobs")) || [];
+
+  function updateJobList() {
+    jobList.innerHTML = "";
+    savedJobs.forEach(function(job) {
       const jobItem = document.createElement("div");
       jobItem.classList.add("job-item");
       jobItem.innerHTML = `
-        <h3>${jobTitle}</h3>
-        <p>${jobDescription}</p>
-        <p>Názov firmy: ${companyName}</p>
-        <p>Telefónne číslo: ${phoneNumber}</p>
+        <h3>${job.jobTitle}</h3>
+        <p>${job.jobDescription}</p>
+        <p>Názov firmy: ${job.companyName}</p>
+        <p>Telefónne číslo: ${job.phoneNumber}</p>
       `;
-  
       jobList.appendChild(jobItem);
-  
-      jobForm.reset();
     });
+  }
+
+  jobForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const jobTitle = document.getElementById("jobTitle").value;
+    const jobDescription = document.getElementById("jobDescription").value;
+    const companyName = document.getElementById("companyName").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+
+    const newJob = {
+      jobTitle: jobTitle,
+      jobDescription: jobDescription,
+      companyName: companyName,
+      phoneNumber: phoneNumber
+    };
+
+    savedJobs.push(newJob);
+    localStorage.setItem("jobs", JSON.stringify(savedJobs));
+    updateJobList();
+    jobForm.reset();
   });
-  
+
+  updateJobList();
+});
