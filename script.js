1e58cd1850aa396fa4632494ffbc92ed3d3768a5
emajasalova <143315521+emajasalova@users.noreply.github.com>
2023-08-30 22:17:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
 document.addEventListener("DOMContentLoaded", function() {
   const jobForm = document.getElementById("jobForm");
   const jobList = document.querySelector(".job-list");
-  const savedJobs = JSON.parse(localStorage.getItem("jobs")) || [];
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQ3b6fw8GqTOkrkOYgoOHuYIh4Ke4H_kw",
+    authDomain: "githubproject-6def0.firebaseapp.com",
+    projectId: "githubproject-6def0",
+    storageBucket: "githubproject-6def0.appspot.com",
+    messagingSenderId: "621024229301",
+    appId: "1:621024229301:web:e7f960f8c5d008c518cbfc",
+    measurementId: "G-H2Z2HGLQK5"
+  };
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
 
   function updateJobList() {
     jobList.innerHTML = "";
-    savedJobs.forEach(function(job) {
-      const jobItem = document.createElement("div");
-      jobItem.classList.add("job-item");
-      jobItem.innerHTML = `
-        <h3>${job.jobTitle}</h3>
-        <p>${job.jobDescription}</p>
-        <p>Názov firmy: ${job.companyName}</p>
-        <p>Telefónne číslo: ${job.phoneNumber}</p>
-      `;
-      jobList.appendChild(jobItem);
-    });
+    database.ref("ponuky").once("value")
+      .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          const ponuka = childSnapshot.val();
+          const jobItem = document.createElement("div");
+          jobItem.classList.add("job-item");
+          jobItem.innerHTML = `
+            <h3>${ponuka.nadpis}</h3>
+            <p>${ponuka.popis}</p>
+            <p>Názov firmy: ${ponuka.firma}</p>
+            <p>Telefónne číslo: ${ponuka.telefon}</p>
+          `;
+          jobList.appendChild(jobItem);
+        });
+      });
   }
 
   jobForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const jobTitle = document.getElementById("jobTitle").value;
-    const jobDescription = document.getElementById("jobDescription").value;
-    const companyName = document.getElementById("companyName").value;
-    const phoneNumber = document.getElementById("phoneNumber").value;
+    const nadpis = document.getElementById("jobTitle").value;
+    const popis = document.getElementById("jobDescription").value;
+    const firma = document.getElementById("companyName").value;
+    const telefon = document.getElementById("phoneNumber").value;
 
-    const newJob = {
-      jobTitle: jobTitle,
-      jobDescription: jobDescription,
-      companyName: companyName,
-      phoneNumber: phoneNumber
+    const novaPonuka = {
+      nadpis: nadpis,
+      popis: popis,
+      firma: firma,
+      telefon: telefon
     };
 
-    savedJobs.push(newJob);
-    localStorage.setItem("jobs", JSON.stringify(savedJobs));
+    // Ukladanie do Firebase
+    database.ref("ponuky").push(novaPonuka);
     updateJobList();
     jobForm.reset();
   });
 
   updateJobList();
 });
+</script>
+
