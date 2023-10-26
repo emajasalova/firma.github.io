46c05df5db86779b5e2eacacaddce87fb8d03911
emajasalova <143315521+emajasalova@users.noreply.github.com>
2023-10-26 22:45:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,31 +7,10 @@ participants.push('Meno7');
 participants.push('Meno8');
 // Môžete pridať ďalšie mená účastníkov podľa potreby
 
-// Odpočet 16 dní
-const endDate = new Date();
-endDate.setDate(endDate.getDate() + 16);
 
-const countdown = setInterval(function() {
-  const now = new Date().getTime();
-  const distance = endDate - now;
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById('countdownText').innerHTML = `Odpočet: ${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-  if (distance < 0) {
-    clearInterval(countdown);
-    if (participants.length > 0) {
-      const winner = participants[Math.floor(Math.random() * participants.length)];
-      document.getElementById('countdownText').innerHTML = `Víťaz: ${winner}`;
-    } else {
-      document.getElementById('countdownText').innerHTML = 'Nikto nevyhral, nikto sa neregistroval.';
-    }
-  }
-}, 1000);
 
 function renderParticipants() {
   const participantsDiv = document.getElementById('participants');
