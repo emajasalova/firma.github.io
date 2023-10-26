71cd05a5cebff7ab2e6b1e8f96160d69453cba25
emajasalova <143315521+emajasalova@users.noreply.github.com>
2023-10-26 22:25:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -49,8 +49,11 @@ const countdown = setInterval(function() {
   const distance = endDate - now;
 
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById('countdownText').innerHTML = `Odpočet: ${days} dní`;
+  document.getElementById('countdownText').innerHTML = `Odpočet: ${days}d ${hours}h ${minutes}m ${seconds}s`;
 
   if (distance < 0) {
     clearInterval(countdown);
