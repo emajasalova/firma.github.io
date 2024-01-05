6b41b868ee6ca1a9ab545b593a308d3c90bcdcb2
emajasalova <143315521+emajasalova@users.noreply.github.com>
2024-01-06 00:03:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-$("#contactForm").submit(function(event) 
-     {
-         /* stop form from submitting normally */
-         event.preventDefault();
-
-         /* get some values from elements on the page: */
-         var $form = $( this ),
-             $submit = $form.find( 'button[type="submit"]' ),
-             name_value = $form.find( 'input[name="name"]' ).val(),
-             email_value = $form.find( 'input[name="email"]' ).val(),
-             message_value = $form.find( 'textarea[name="message"]' ).val(),
-             url = $form.attr('action');
-
-         /* Send the data using post */
-         var posting = $.post( url, { 
-                           name: name_value, 
-                           email: email_value, 
-                           message: message_value 
-                       });
-
-         posting.done(function( data )
-         {
-             /* Put the results in a div */
-             $( "#contactResponse" ).html(data);
-
-             /* Change the button text. */
-             $submit.text('Sent, Thank you');
-
-             /* Disable the button. */
-             $submit.attr("disabled", true);
-         });
-    });
+function thankYou() {
+    window.alert('Děkujeme za vaši žádost brzy ji provaříme a dáme vám vědět !');
+}
